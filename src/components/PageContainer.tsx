7509c15ebfdd08e3a92e469684b633992bef5357
enoatu <acro1452@gmail.com>
2021-06-05 15:13:03 +0900

    :wrench: typescript

---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from 'react'
+import Head from 'next/head'
+import Footer from '@c/Footer'
+
+type Props = {
+  children?: ReactNode
+  title?: string
+}
+
+export default function PageContainer({ children, title = 'This is the default title' }: Props) {
+  return (
+    <div className="container">
+      <Head>
+        <title>DevUtils | { title }</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      { children }
+
+      <Footer/>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,88 +1,42 @@
-import Head from 'next/head'
+import PageContainer from '@c/PageContainer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>DevUtils</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <PageContainer title="Top">
       <main>
         <h1 className="title">DevUtils</h1>
         <p className="description">Development Utils</p>
         <div className="grid">
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="/replacer" className="card">
+            <h3>Replacer &rarr;</h3>
+            <p>Simple Text Replacer For Efficient</p>
           </a>
-
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="./combination"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
+            <h3>Combination &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="./multi-replacer"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>MultiReplacer &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
         main {
           padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
           justify-content: center;
           align-items: center;
         }
@@ -168,32 +122,13 @@ export default function Home() {
           line-height: 1.5;
         }
 
-        .logo {
-          height: 1em;
-        }
-
         @media (max-width: 600px) {
           .grid {
             width: 100%;
             flex-direction: column;
           }
         }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+     `}</style>
+    </PageContainer>
   )
 }
