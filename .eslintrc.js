c0f2c24d3cfa023f848e953c52ad1f1265bfec4c
enoatu <acro1452@gmail.com>
2021-06-21 12:45:58 +0900

    :wrench: add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: 2021,
+    ecmaVersion: 12,
     sourceType: 'module'
   },
   plugins: [
@@ -27,5 +27,9 @@ module.exports = {
     'prefer-const': 'error',
     // === の使用を推奨
     eqeqeq: 'error',
+    // import React from 'react'React していないと 'React must be in scope when using JSX.'が出る
+    // 設定で 出さないように https://ja.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
   }
 }
